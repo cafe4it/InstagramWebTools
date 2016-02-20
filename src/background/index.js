b56146283dcff46a9981fe913c84ac176e83611c
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-02-20 12:54:40 +0700

    90%

---
+++ src/background/index.js
@@ -0,0 +1,89 @@
+//import welcome from 'shared/welcome'
+
+//welcome('background/index.js');
+
+let MEDIA = undefined;
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+    if(msg.action === 'show-contextMenuInstagram'){
+        var subTitle = (msg.data.type === 'VIDEO') ? 'video' : 'image';
+        MEDIA = msg.data;
+        console.info(MEDIA);
+        chrome.contextMenus.create({
+            id : 'showContextMenuInstagram_SaveAs',
+            title :  "Save "+subTitle+" as...",
+            contexts : ["all"]
+        })
+        chrome.contextMenus.create({
+            id : 'showContextMenuInstagram_CopyURL',
+            title :  "Copy "+subTitle+" URL",
+            contexts : ["all"]
+        })
+        chrome.contextMenus.create({
+            id : 'showContextMenuInstagram_OpenInNewTab',
+            title : "Open "+subTitle+" in new tab",
+            contexts : ["all"]
+        })
+    }else if(msg.action === 'remove-contextMenuInstagram'){
+        chrome.contextMenus.removeAll();
+    }
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+    if(!MEDIA) return;
+    if(info.menuItemId === 'showContextMenuInstagram_SaveAs'){
+        chrome.downloads.download({url : MEDIA.src});
+    }else if(info.menuItemId === 'showContextMenuInstagram_CopyURL'){
+
+    }else if(info.menuItemId === 'showContextMenuInstagram_OpenInNewTab'){
+        chrome.tabs.create({url : MEDIA.src});
+    }
+})
+
+// Setting popup icon
+
+// When we defined browser_action
+if(chrome.browserAction) {
+  chrome.browserAction.setIcon({
+    path: require("icons/webpack-38.png")
+  })
+
+// When we defined page_action
+} else if(chrome.pageAction) {
+
+  const showPageAction = function(tabId) {
+    chrome.pageAction.show(tabId);
+
+    chrome.pageAction.setIcon({
+      path: require("icons/webpack-38.png"),
+      tabId: tabId
+    })
+  }
+
+    chrome.runtime.onInstalled.addListener(function() {
+        // Replace all rules ...
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+            // With a new rule ...
+            chrome.declarativeContent.onPageChanged.addRules([
+                {
+                    // That fires when a page's URL contains a 'g' ...
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher({
+                            pageUrl: {    hostEquals: 'instagram.com' },
+                        })
+                    ],
+                    // And shows the extension's page action.
+                    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+                }
+            ]);
+        });
+    });
+
+
+  // Show page action on each page update
+/*  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    showPageAction(tabId)
+  });*/
+}
+
+// When the extension is installed or upgraded ...
---
+++ src/content/index.js
@@ -0,0 +1,200 @@
+require('clipboard');
+
+var _IS_DETAIL_PAGE = false;
+
+
+$(document).on('ready',function(){
+//Clipboard
+    var btnClipboard = document.createElement('button');
+    btnClipboard.id = 'btnClipboard';
+    btnClipboard.style.cssText = 'display : none!important;width : 0px !imporant; height : 0px !important;';
+    document.body.appendChild(btnClipboard);
+    new Clipboard('#btnClipboard');
+//Clipboard
+
+//SaveAs
+    var btnSaveAs = document.createElement('a');
+    btnSaveAs.id = 'btnSaveAs';
+    btnSaveAs.style.cssText = 'display : none!important;width : 0px !imporant; height : 0px !important;';
+    document.body.appendChild(btnSaveAs);
+    $(btnSaveAs).on('click', debounce(function (event) {
+        console.log('download.')
+    }, 500));
+
+    $(document.body).on('mouseenter','a._8mlbc,article > div._22yr2, div._rudo5, a._c2kdw',function(e){
+        var context = $(this).parent().context;
+        var elem = (!$(context).hasClass('_c2kdw')) ? context : $(this).parent();
+        //if($(elem).find('._1lp5e').length > 0) return; //video
+        //console.log(elem,e.target);
+        showMenuContext(elem);
+        //chrome.runtime.sendMessage({elem : e.target});
+    })
+})
+
+
+
+
+/*----Extend------*/
+String.prototype.replaceArray = function(find, replace) {
+    var replaceString = this;
+    var regex;
+    for (var i = 0; i < find.length; i++) {
+        regex = new RegExp(find[i], "g");
+        replaceString = replaceString.replace(regex, replace[i]);
+    }
+    return replaceString;
+};
+
+function getPathFromUrl(url) {
+    return url.split(/[?#]/)[0];
+}
+
+function getExtensionFromUrl(url){
+    return (/[.]/.exec(url)) ? /[^.]+$/.exec(url) : undefined;
+}
+
+function getFileNameFromUrl(url){
+    var path = getPathFromUrl(url);
+    return path.split("/").pop();
+}
+
+function validateMediaSrc(mediaUrl){
+    var _filter_find = ['/s640x640','/s750x750'],
+        _filter_replace = ['',''];
+    return mediaUrl.replaceArray(_filter_find, _filter_replace);
+}
+
+function getMediaSrc(elem){
+    var rs = $(elem).find('img[id^="pImage_"], video');
+    if(rs.length > 0){
+        var _media = rs[0],
+            _mediaSrc = $(_media).attr('src') || null,
+            _mediaType = $(rs[0]).is('video') ? 'VIDEO' : 'IMAGE' || null;
+
+        _mediaSrc = validateMediaSrc(_mediaSrc);
+        return {
+            _mediaSrc : _mediaSrc,
+            _mediaType : _mediaType,
+            _mediaName : getFileNameFromUrl(_mediaSrc)
+        }
+    }
+    return null;
+}
+
+function throttle(func, interval) {
+    var lastCall = 0;
+    return function() {
+        var now = Date.now();
+        if (lastCall + interval < now) {
+            lastCall = now;
+            return func.apply(this, arguments);
+        }
+    };
+}
+
+function debounce(func, interval) {
+    var lastCall = -1;
+    return function () {
+        clearTimeout(lastCall);
+        var args = arguments;
+        lastCall = setTimeout(function () {
+            func.apply(this, args);
+        }, interval);
+    };
+}
+
+function getVideoUrl(postUrl){
+    var defer = $.Deferred();
+    defer.then(function(){
+        appAPI.request.get({
+            url : postUrl,
+            onSuccess : function(){
+
+            }
+        })
+    })
+}
+
+function showMenuContext(elem){
+    var _mediaSrc = null, _mediaType = null;
+    if($(elem).find('._1lp5e').length > 0){
+        var postUrl = ($(elem).is('a')) ? $(elem).attr('href') : null;
+        var res = $.ajax({
+            type: "GET",
+            url: postUrl + '&__a=1',
+            async: false
+        }).responseText;
+        res = $.parseJSON(res);
+        if(res.media && res.media.is_video === true){
+            _mediaSrc = res.media.video_url;
+            _mediaType = 'VIDEO';
+        }
+    }else{
+        var rs = getMediaSrc(elem);
+        if(rs){
+            _mediaSrc = rs._mediaSrc,
+                _mediaType = rs._mediaType
+        }
+    }
+
+    if((_mediaSrc && _mediaType) === false) return;
+    chrome.runtime.sendMessage({action:'show-contextMenuInstagram',
+        data : {
+            src : _mediaSrc,
+            type : _mediaType
+        }});
+
+    console.info(_mediaSrc, _mediaType);
+
+    if(_IS_DETAIL_PAGE){
+        $('#btnClipboard').attr('data-clipboard-text', _mediaSrc);
+        $('#btnSaveAs').attr({'href' :  _mediaSrc, 'download' : getFileNameFromUrl(_mediaSrc)});
+    }else{
+        $(elem).on('contextmenu',function(){
+            $('#btnClipboard').attr('data-clipboard-text', _mediaSrc);
+            $('#btnSaveAs').attr({'href' :  _mediaSrc, 'download' : getFileNameFromUrl(_mediaSrc)});
+        })
+    }
+
+
+    $(elem).mouseleave(function(){
+        //$('#btnClipboard').attr('data-clipboard-text', null);
+        //$('#btnSaveAs').attr({'download' :  null,'href' : null});
+        removeMenuContext();
+    })
+
+}
+
+function removeMenuContext(){
+    chrome.runtime.sendMessage({action:'remove-contextMenuInstagram'});
+}
+
+/*----Extend------*/
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+    if(msg.action === 'tabUpdated'){
+        if(window.location.href === msg.data.pageUrl && msg.data.requestMethod === 'GET'){
+
+            if(appAPI.isMatchPages("*.instagram.com/p/*")){
+                appAPI.pageAction.show();
+                _IS_DETAIL_PAGE = true;
+                var rs = getMediaSrc($, 'article._j5hrx');
+                if(rs._mediaSrc){
+                    $('#btnSaveAs').attr({
+                        href : rs._mediaSrc,
+                        download : rs._mediaName
+                    })
+                }
+            }else{
+                appAPI.pageAction.hide();
+                _IS_DETAIL_PAGE = false;
+            }
+        }
+    } else if(msg.action === 'copyURL'){
+        if(!$('#btnClipboard').attr('data-clipboard-text')) return;
+        $('#btnClipboard').click();
+    } else if(msg.action === 'saveAs'){
+        if(!$('#btnSaveAs').attr('download')) return;
+        document.getElementById('btnSaveAs').click();
+    }
+})
